8f3a5c6e154748a2bc47a35ca8a0b34dcce38660
kmadden84 <49003301+kmadden84@users.noreply.github.com>
2019-06-18 23:52:40 -0400

    page toggle buttons
    basic next/prev buttons

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
 import Search from './Search';
 
+var page = "null";
 
 export default class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       content: "",
-
-      nocontent: false
+      query: "",
+      nocontent: false,
+      endoflist: false,
+      page: 1
     }
-    this.setSearchStatee = this.setSearchState.bind(this);
+    this.setSearchState = this.setSearchState.bind(this);
+    // this.pageChange = this.pageChange.bind(this);
+
+
   }
   componentDidMount(props) {
   }
 
+
+  pageChange = (type) => {
+    if (type == 'add' && !this.state.nocontent) {
+      this.setState(prevState => {
+        return { page: prevState.page + 1 }
+      });
+    }
+    if (type == 'sub' && this.state.page >= 2) {
+      this.setState(prevState => {
+        return { page: prevState.page - 1 }
+      });
+    }
+    console.log(this.state.page)
+    this.setSearchState(this.state.query);
+  }
+
   setSearchState = (query) => {
-    var url = "https://opentable.herokuapp.com/api/restaurants?city=" + query;
+
+    var page = this.state.page;
+    var url = "https://opentable.herokuapp.com/api/restaurants?city=" + query + "&per_page=25&page=" + page;
+    console.log(url)
     return new Promise((resolve, reject) => {
       fetch(url, {
         method: "GET",
@@ -24,14 +49,21 @@ export default class SearchPage extends Component {
       })
         .then((response) => {
           response.json().then((responseJson) => {
+            console.log(responseJson)
             resolve(responseJson)
             this.setState({
-              content: responseJson.restaurants,
-              nocontent: false
+              content: responseJson,
+              nocontent: false,
+              query: query
             })
-            if (responseJson.total_entries === 0)
+            if (responseJson && responseJson.restaurants == 0) {
               this.setState({
                 nocontent: true
+              })
+            }
+            if (responseJson && responseJson.restaurants== 0 && this.state.page > 2)
+              this.setState({
+                endoflist: true
               })
           })
         })
@@ -46,54 +78,76 @@ export default class SearchPage extends Component {
 
     var restaurantTile = [];
 
-    if (this.state.content) {
 
-    for (var i = 0; i < this.state.content.length; i++) {
-      var name = this.state.content[i].name;
-      var city = this.state.content[i].city;
-      var state = this.state.content[i].state;
-      var country = this.state.content[i].country;
-      var address = this.state.content[i].address + ', ' + city + ', ' + state + ', ' + country;
-      var url = this.state.content[i].reserve_url;
-      var price;
-      switch (this.state.content[i].price) {
-        case 1:
-          price = "$"
-          break;
-        case 2:
-          price = "$$"
-          break;
-        case 3:
-          price = "$$$"
-          break;
-        case 4:
-          price = "$$$$"
-          break;
-        default:
+    if (this.state.content.restaurants) {
+
+      for (var i = 0; i < this.state.content.restaurants.length; i++) {
+        var name = this.state.content.restaurants[i].name;
+        var city = this.state.content.restaurants[i].city;
+        var state = this.state.content.restaurants[i].state;
+        var country = this.state.content.restaurants[i].country;
+        var address = this.state.content.restaurants[i].address + ', ' + city + ', ' + state + ', ' + country;
+        var url = this.state.content.restaurants[i].reserve_url;
+        var price;
+        switch (this.state.content.restaurants[i].price) {
+          case 1:
+            price = "$"
+            break;
+          case 2:
+            price = "$$"
+            break;
+          case 3:
+            price = "$$$"
+            break;
+          case 4:
+            price = "$$$$"
+            break;
+          default:
+        }
+        restaurantTile.push(
+          <div className="grid-33" key={i}>
+            <div class="innerContent">
+              <h4 className="rest--name"><a href={url} target="_blank">{name}</a></h4>
+              <h3 className="rest--add">Address: {address}</h3>
+              <h3 className="rest--price">Cost: {price}</h3></div>
+          </div>
+        )
       }
-      restaurantTile.push(
-        <div className="grid-33" key={i}>
-          <div class="innerContent">
-            <h4 className="rest--name"><a href={url} target="_blank">{name}</a></h4>
-            <h3 className="rest--add">Address: {address}</h3>
-            <h3 className="rest--price">Cost: {price}</h3></div>
-
-        </div>
-      )
     }
-  }
+    const total_pages = this.state.content.total_entries;
+    const per_page = this.state.content.per_page;
+    let pages = total_pages / per_page;
+    pages = Math.ceil(pages);
+    //pages = parseInt(pages, 10) + 1;
+    //console.log(typeof(pages))
+    //var foo = new Array(pages).fill(0);
+    //console.log(foo.length)
+
     return (
       <div className="container">
-      <h1 class="mainHeader">OpenTable Restaurant Search</h1>
+        <h1 class="mainHeader">OpenTable Restaurant Search</h1>
         <Search data={this.setSearchState} />
         <div class="results">
 
           {
-            (this.state.nocontent)
+            (this.state.nocontent && !this.state.endoflist)
               ? <p className="noresults">No results. Please try another city.</p>
               : ""
           }
+          {
+            (this.state.endoflist)
+              ? <div><p className="noresults">please hit Prev you've gone too far.</p>
+                <button onClick={this.pageChange.bind(this, 'sub')} className="bottom">Prev </button></div>
+              : ""
+          }
           {restaurantTile}
+
+          {
+            (this.state.query && !this.state.nocontent || !this.state.nocontent && this.state.endoflist)
+              ? <div><button onClick={this.pageChange.bind(this, 'add')} className="bottom">Next </button>
+                <button onClick={this.pageChange.bind(this, 'sub')} className="bottom">Prev </button></div>
+              : ""
+          }
         </div>
       </div>
     );
