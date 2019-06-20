fffb0351e98fe587d86a7bc885dc42a514749799
kmadden84 <49003301+kmadden84@users.noreply.github.com>
2019-06-20 13:59:30 -0400

    Update SearchPage.js

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -42,9 +42,7 @@ export default class SearchPage extends Component {
 
   }
   setSearchState = (query) => {
-    this.setState({
-      page: 1
-    })
+
     var page = this.state.page;
     var url = "https://opentable.herokuapp.com/api/restaurants?city=" + query + "&per_page=25&page=" + page;
     console.log(url)
